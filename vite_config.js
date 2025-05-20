11fa1272b4b293de1a2d3397842cd5ee64268d80
Carl + Angela Nicolson <hello@klaario.com>
2025-05-19 18:54:52 -0700

    Update vite_config.js

--- vite_config.js
+++ vite_config.js
@@ -5,11 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  base: './',
+    sourcemap: true
+  }
 })
